63553dc796af782279212fd878c8bc2cdb91ac1a
Demilade Omitiran <omitirand@gmail.com>
2024-05-26 08:13:51 +0100

    Add name to router init params

--- src/router.py
+++ src/router.py
@@ -1,5 +1,6 @@
 class Router:
-  def __init__(self, router_id, location_id, links):
+  def __init__(self, router_id, name, location_id, links):
     self.router_id = router_id
+    self.name = name
     self.location_id = location_id
     self.links = links
