d0551c9841650738d8bc4b7d0410c60c92d24c1e
Demilade Omitiran <omitirand@gmail.com>
2024-05-26 08:09:37 +0100

    Add postcode to location init params

--- src/location.py
+++ src/location.py
@@ -1,4 +1,5 @@
 class Location:
-  def __init__(self, location_id, name):
+  def __init__(self, location_id, name, postcode):
     self.location_id = location_id
     self.name = name
+    self.postcode = postcode
--- tests/test_location.py
+++ tests/test_location.py
@@ -2,11 +2,12 @@ import unittest
 
 from src.location import Location
 
-class TestRouter(unittest.TestCase):
+class TestLocation(unittest.TestCase):
   def test_location_initialization(self):
-    location = Location(1, "Birmingham")
+    location = Location(1, "Birmingham", "B21 ABC")
     self.assertEqual(location.location_id, 1)
     self.assertEqual(location.name, "Birmingham")
+    self.assertEqual(location.postcode, "B21 ABC")
 
 if __name__ == "__main__":
   unittest.main()
