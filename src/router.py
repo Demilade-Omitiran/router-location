66d7459032d18e712f9639279326d4db8e5e6ff1
Demilade Omitiran <omitirand@gmail.com>
2024-05-26 08:44:11 +0100

    Update router_id to id

--- src/router.py
+++ src/router.py
@@ -1,6 +1,6 @@
 class Router:
   def __init__(self, router_id, name, location_id, links):
-    self.router_id = router_id
+    self.id = router_id
     self.name = name
     self.location_id = location_id
     self.links = links
--- tests/test_router.py
+++ tests/test_router.py
@@ -5,7 +5,7 @@ from src.router import Router
 class TestRouter(unittest.TestCase):
   def test_router_initialization(self):
     router = Router(1, "This Router", 1, [2, 3])
-    self.assertEqual(router.router_id, 1)
+    self.assertEqual(router.id, 1)
     self.assertEqual(router.name, "This Router")
     self.assertEqual(router.location_id, 1)
     self.assertEqual(router.links, [2, 3])
